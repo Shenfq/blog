406581a2b55c59fbe61d2073e529ebf57defa963
Shenfq <shenfq95@foxmail.com>
2021-04-12 10:56:26 +0800

    feat: 配置加入 head

--- pagic.config.tsx
+++ pagic.config.tsx
@@ -1,9 +1,24 @@
+import { React } from 'https://deno.land/x/pagic@v1.2.0/mod.ts';
+
 export default {
   theme: 'blog',
   title: '自然醒的博客',
   description: '前端工程师@拼多多，爱折腾，擅长 JavaScript，欢迎关注我的公众号「更了不起的前端」',
   srcDir: 'blog',
   plugins: ['blog'],
+  head: (
+    <>
+      <script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?681a9f14f2db8e622747ef03c33bb367";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+      </script>
+    </>
+  ),
   blog: {
     root: '/posts/',
     author: 'shenfq',
